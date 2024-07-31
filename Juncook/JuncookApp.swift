a84f2a883c60fc2e9aad87751e2937a3e19625b0
Tuyênnn <55830905+MrTuyennn@users.noreply.github.com>
2024-07-31 22:39:46 +0700

    add example camera

---
+++ Juncook/JuncookApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuncookApp.swift
+//  Juncook
+//
+//  Created by Nguyen Ngoc Tuyen on 31/7/24.
+//
+
+import SwiftUI
+
+@main
+struct JuncookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CameraView()
+        }
+    }
+}
