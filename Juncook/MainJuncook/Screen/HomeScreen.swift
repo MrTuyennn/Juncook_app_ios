98a6ce3da499a3221aeb4802b5a213807f21cbcf
Tuyênnn <55830905+MrTuyennn@users.noreply.github.com>
2024-09-03 22:18:50 +0700

    learn header screen - toolbar -

--- Juncook/MainJuncook/Screen/HomeScreen.swift
+++ Juncook/MainJuncook/Screen/HomeScreen.swift
@@ -10,35 +10,43 @@ import SwiftUI
 
 struct HomeScreen: View {
     
-    
-    
-    
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Welcome to Home").foregroundStyle(Color.orange)
-                    .padding()
+                ScrollView(.vertical) {
+                    LazyVStack(spacing: 0) {
+                        ForEach(1...500, id: \.self) { number in
+                            Text("\(number)")
+                                .foregroundColor(.blue)
+                        }
+                    }
+                }
+                .background(Color.black)
+                .scrollIndicators(.hidden)
+                .padding(0)
             }
             .toolbar {
                 ToolbarItem(placement: .principal) {
-                    HStack {
-                        Text("Home")
-                            .font(.title)
-                            .foregroundColor(.orange)
-                        Spacer()
-                        Text("Home")
-                            .font(.title)
-                            .foregroundColor(.orange)
-                        Spacer()
-                        Text("Home")
-                            .font(.title)
-                            .foregroundColor(.orange)
-                    }
+                    Text("Home")
+                        .font(.title)
+                        .foregroundColor(.orange)
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    Text("Home")
+                        .font(.title)
+                        .foregroundColor(.orange)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Text("Home")
+                        .font(.title)
+                        .foregroundColor(.orange)
                 }
             }
             .preferredColorScheme(.dark)
             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight: .infinity)
             .background(Color.black)
+            .toolbarTitleDisplayMode(.inline)
+            .navigationBarTitleDisplayMode(.inline)
         }
         
     }
